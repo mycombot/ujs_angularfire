ad05a0db99eebe3aaee20bc0529ffb6bc6323f1b
Ray Villalobos <ray@planetoftheweb.com>
2014-10-14 04:04:44 -0400

    added firebase

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute','firebase']);
 
 myApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,8 +1,20 @@
 myApp.controller('RegistrationController', 
-  function($scope, $location) {
+  function($scope, $location, $firebase) {
+
+  var ref = new Firebase('https://regtest100.firebaseio.com/' + 'users');
+  var firebaseUsers = $firebase(ref);
 
   $scope.register = function() {
-    $location.path('/thanks');
-  } //register
+    var userInfo = {
+      date: Firebase.ServerValue.TIMESTAMP,
+      name: $scope.user.myname,
+      email: $scope.user.myEmail
+    }
 
+    firebaseUsers.$push(userInfo)
+    	.then(function(ref) {
+		    $location.path('/thanks');
+		    console.log(ref.name());
+			});
+  } //register
 }); //RegistrationController
